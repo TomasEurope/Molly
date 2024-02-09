eee4f3122d89be68fc1fff8d5f0e6864f65d2013
Tomas <studnasoft@gmail.com>
2024-02-09 17:23:28 +0100

    HotFix IPs range

--- app/Services/MollyService.php
+++ app/Services/MollyService.php
@@ -20,8 +20,8 @@ class MollyService implements ServiceInterface
 
     final public function getIpsRandom(int $count = 1, ?string $where = null): array
     {
-        return $this->db->fetchRowMany('SELECT id, ip FROM ips ' . ($where ? 'WHERE ' . $where : '') . ' ORDER BY updated_at ASC LIMIT :count OFFSET :offset'
-            , ['count' => $count, 'offset' => rand(0, 5) * $count ]);
+        return $this->db->fetchRowMany('SELECT id, ip FROM ips ' . ($where ? 'WHERE ' . $where : '') . ' ORDER BY updated_at ASC LIMIT :count'
+            , ['count' => $count]);
     }
 
     final public function markUpdated(string $table, array $ids){
